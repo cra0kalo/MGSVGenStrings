1ad54b5ecfc3589d5e43e60c30fffa9cf3dd9969
Cra0kalo <me@cra0kalo.com>
2015-09-07 20:59:23 +1000

    Initial commit with project files

---
+++ genstr/genstr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genstr
+{
+
+    using genstr.FOXEngine;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
